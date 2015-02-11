6788a144d0b8671399f279082ce9148763e7bf7e
dmitrykudrenko <dmitrykudrenko@97baf54d-214b-41ac-aef7-15a7bd0d0312>
2015-02-11 08:35:31 +0000

    remove useless resources

--- dolphin/src/main/java/com/dolphin/component/tabs/SlidingTabsBasicFragment.java
+++ dolphin/src/main/java/com/dolphin/component/tabs/SlidingTabsBasicFragment.java
@@ -43,7 +43,7 @@ public abstract class SlidingTabsBasicFragment extends SimpleFragment implements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sample, container, false);
+        return inflater.inflate(R.layout.sliding_tab_fragment, container, false);
     }
 
     @Override
