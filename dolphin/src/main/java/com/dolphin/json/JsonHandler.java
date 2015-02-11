0ac96647258fd2039fc0119714d83bdaae423691
dmitrykudrenko <dmitrykudrenko@97baf54d-214b-41ac-aef7-15a7bd0d0312>
2015-02-11 08:41:39 +0000

    refactoring jsonHandler

--- dolphin/src/main/java/com/dolphin/json/JsonHandler.java
+++ dolphin/src/main/java/com/dolphin/json/JsonHandler.java
@@ -11,7 +11,6 @@ import com.google.gson.JsonParseException;
 import com.google.gson.reflect.TypeToken;
 
 import org.apache.commons.lang3.StringUtils;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Type;
@@ -25,10 +24,10 @@ import java.util.Map;
 
 
 public class JsonHandler<T> {
+    public static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
 
     private static Gson gsonParser = new GsonBuilder()
             .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-//            .setDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
             .registerTypeAdapter(Date.class, new DateSerializer())
             .create();
 
@@ -39,33 +38,24 @@ public class JsonHandler<T> {
     }
 
     public JSONObject toJsonObject(T object) throws Throwable {
-
-        try {
-            return new JSONObject(toJsonString(object));
-        }
-        catch (JSONException e) {
-            throw new Throwable();
-        }
+        return new JSONObject(toJsonString(object));
     }
 
     public String toJsonString(T object) {
-
         return gsonParser.toJson(object, mClass);
     }
 
     public T fromJsonObject(JSONObject object) {
-
         return gsonParser.fromJson(object.toString(), mClass);
     }
 
     public T fromJsonString(String jsonString) {
-
         return gsonParser.fromJson(jsonString, mClass);
     }
 
     public List<T> listFromJsonString(String jsonString) {
-
-        Type listOfTestObject = new TypeToken<List<T>>(){}.getType();
+        Type listOfTestObject = new TypeToken<List<T>>() {
+        }.getType();
         List<Map> maps = gsonParser.fromJson(jsonString, listOfTestObject);
         List<T> result = new ArrayList<T>(maps.size());
         for (Map map : maps) {
@@ -76,14 +66,14 @@ public class JsonHandler<T> {
     }
 
     public String listToJsonString(List<T> list) {
-        JsonElement element = gsonParser.toJsonTree(list, new TypeToken<List<T>>() {}.getType());
-
+        JsonElement element = gsonParser.toJsonTree(list, new TypeToken<List<T>>() {
+        }.getType());
         return element.getAsJsonArray().toString();
     }
 
     public Map<String, String> toMap(T object) {
-        String s = toJsonString(object);
-        return gsonParser.fromJson(s, new TypeToken<HashMap<String, String>>() {}.getType());
+        return gsonParser.fromJson(toJsonString(object), new TypeToken<HashMap<String, String>>() {
+        }.getType());
     }
 
     private static class DateSerializer implements JsonDeserializer<Date> {
@@ -92,7 +82,7 @@ public class JsonHandler<T> {
             String date = json.getAsString();
             if (StringUtils.isNoneBlank(date)) {
                 try {
-                    return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(date);
+                    return new SimpleDateFormat(DATE_FORMAT).parse(date);
                 } catch (ParseException e) {
                     return null;
                 }
